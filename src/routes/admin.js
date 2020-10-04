a1c3c0717184f74916cfa11e432fbcc6552b032c
CF12 <brianx0011@gmail.com>
2020-10-04 13:27:56 -0400

    Add receipts section to tx page

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -55,13 +55,13 @@ app.get('/transactions', (req, res) => {
 
 app.get('/transactions/:id', (req, res) => {
   db.findTxById(req.params.id).then((d) => {
-    if (d['Receipt']) {
-      d['Receipt'] = d['Receipt'][0]['url']
-    }
+    const receipts = d['Receipt']
+    delete d['Receipt']
 
     res.render('admin/transaction-single', {
       title: `Tx #${d['Tx ID']}`,
-      tx: d
+      tx: d,
+      receipts
     })
   })
 })
